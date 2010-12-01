2d64e5cf7a0d09d16e4d8e3b31c6115e843b31ab
Gavin Gilmour <gavin.gilmour@gmail.com>
2010-12-01 00:39:11 +0000

    start of info panel

---
+++ Classes/TorrentInfoController.h
@@ -0,0 +1,16 @@
+//
+//  TorrentInfoController.h
+//  Tlaloc
+//
+//  Created by Gavin Gilmour on 30/11/2010.
+//  Copyright 2010 SPSA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TorrentInfoController : NSWindowController {
+
+}
+
+@end
---
+++ Classes/TorrentInfoController.m
@@ -0,0 +1,34 @@
+//
+//  TorrentInfoController.m
+//  Tlaloc
+//
+//  Created by Gavin Gilmour on 30/11/2010.
+//  Copyright 2010 SPSA. All rights reserved.
+//
+
+#import "TorrentInfoController.h"
+
+
+@implementation TorrentInfoController
+
+- (id) init
+{
+    self = [super initWithWindowNibName: @"TorrentInfoView"];
+    return self;
+}
+
+- (void) awakeFromNib
+{
+
+    NSLog(@"hello");
+
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver: self];
+    
+    [super dealloc];
+}
+
+@end
